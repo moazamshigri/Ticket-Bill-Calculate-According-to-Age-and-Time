d03b8ae37c12487b54c9c3e9ffe5ff0aefa0fd1e
Muhammad Moazam <124040080+moazamshigri@users.noreply.github.com>
2023-11-20 23:02:01 +0500

    Add files via upload

---
+++ Bill.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+	int age, ticket_prize;
+	char showtime;
+	
+	printf("What is Your Age: ");
+	scanf("%d",&age);
+	printf("\n Chose the Show Time");
+	printf("\n\n 1. Press (capital) M For Morning");
+	printf("\n 2. Press (capital) E For Evering \n \t");
+	scanf(" %c",&showtime);
+	if(age>=18){
+		if(showtime == 'M'){
+			ticket_prize = 12;
+		}
+		else{
+			ticket_prize = 15;
+		}
+		
+				}
+	else{
+			if(showtime == 'M'){
+				
+				ticket_prize = 8;
+		}
+			else{
+			ticket_prize = 10;
+		}
+	}
+	printf(" The total Bill is %d $",ticket_prize);
+	return 0;
+	getch();
+	
+}
